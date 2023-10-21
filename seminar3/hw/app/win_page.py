1f317e9aeba2a97674386ce72a9927beef8e66cd
KassandraKox <sawiii@mail.ru>
2023-10-21 10:14:23 +0300

    seminar + hw 3

---
+++ seminar3/hw/app/win_page.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+
+class WinPage(ft.UserControl):
+    def __init__(self, winner):
+        super().__init__()
+        self.win_text = ft.Text('Пользователь победил!' if winner == 'user' else 'Компьютер победил!',
+                                size=18,
+                                weight="bold"
+                                )
+        self.start_game = ft.ElevatedButton(text='Еще раз?', on_click=lambda _: self.page.go("/game"))
+
+    def build(self):
+        return ft.Column(
+            [
+                self.win_text,
+                self.start_game
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
